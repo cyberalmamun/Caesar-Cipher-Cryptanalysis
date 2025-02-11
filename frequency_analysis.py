a7c3581a09cca74e18ab2609da4c7aee33f3f6f4
Abdullah Almamun <165108040+almamun-de@users.noreply.github.com>
2025-02-11 23:34:32 +0100

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -5,7 +5,3 @@ def frequency_analysis(ciphertext):
     total = sum(counter.values())
     for char, count in counter.items():
         if char.isalpha():
-            print(f"{char}: {count / total:.2%}")
-
-# Example usage
-frequency_analysis(ciphertext)
